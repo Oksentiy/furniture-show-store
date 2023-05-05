677215cd716654d9c2ab4b6763d339bca26ddf4c
Oksentiy <94550267+Oksentiy@users.noreply.github.com>
2023-05-05 13:17:36 +0300

    added popup for auth before make an order

--- src/components/pages/singleProductPage/SingleProductPageComponents/Form.tsx
+++ src/components/pages/singleProductPage/SingleProductPageComponents/Form.tsx
@@ -8,6 +8,7 @@ import {calculatePrice} from "components/pages/singleProductPage/SingleProductPa
 import '../styles/form.scss'
 import {useParams} from "react-router-dom";
 import {sendOrderData} from "components/pages/singleProductPage/SingleProductPageComponents/methods/postFormData";
+import {PopUpForRegistry} from "components/reusableComponents/PopUpifNotRegistr/PopUpForRegistry";
 
 type Props = {
   setPrice: (data: number) => void,
@@ -23,6 +24,7 @@ export const Form = ({setPrice, priceForThickness}: Props) => {
 
   const USER_TOKEN = localStorage.getItem('token');
 
+  const [openAlertPopup, setOpenAlertPopup] = useState<boolean>(false)
   const [finalPrice, setFinalPrice] = useState<number>()
   const [currentColor, setCurrentColor] = useState<string>()
   const [dataForPrice, setDataForPrice] = useState({
@@ -37,8 +39,8 @@ export const Form = ({setPrice, priceForThickness}: Props) => {
     defaultValues: {
       product_color_id: 0,
       product_thickness_id: 0,
-      product_width: 0,
-      product_length: 0,
+      // product_width: '',
+      // product_length: '',
       quantity: 0,
     },
   });
@@ -83,6 +85,7 @@ export const Form = ({setPrice, priceForThickness}: Props) => {
   };
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
+
     reset();
     setDataForPrice((prevProducts) => ({
       ...prevProducts,
@@ -102,110 +105,122 @@ export const Form = ({setPrice, priceForThickness}: Props) => {
       }
     }
     sendOrderData(convertedData, USER_TOKEN)
-    // sendOrderData()
-
-    console.log(convertedData)
+    // мій код тралі валі
+    showAlertPopup()
   };
+  // відкривання / закривання попапа, що просить увійти
+
+  function showAlertPopup () {
+    if (USER_TOKEN !== null && 'undefined'){
+      setOpenAlertPopup(false)
+    }else{
+      setOpenAlertPopup(true)
+    }
+  }
+  //
 
   return (
-    <form className="form-container" autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-      {productData !== undefined &&
-        <>
-          <legend className='legend'>Колір плівки: <strong style={{fontWeight: 600}}>{currentColor}</strong></legend>
-          <div className="radio-input">
-            {productData.colors.map((color, index) =>
-              <div className="label-wrapper" key={index}>
-                <label style={{backgroundColor: `${color.hex}`}}>
+    <>
+      <PopUpForRegistry openAlert={openAlertPopup} setOpenAlertPopup={setOpenAlertPopup}/>
+      <form className="form-container" autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+        {productData !== undefined &&
+          <>
+            <legend className='legend'>Колір плівки: <strong style={{fontWeight: 600}}>{currentColor}</strong></legend>
+            <div className="radio-input">
+              {productData.colors.map((color, index) =>
+                <div className="label-wrapper" key={index}>
+                  <label style={{backgroundColor: `${color.hex}`}}>
+                    <input
+                      type="radio"
+                      id={color.name}
+                      name="value-radio"
+                      value={Number(index)}
+                      onClick={() => setCurrentColor(color.name)}
+                      {...register("product_color_id", {required: true,})}/>
+                    <span/>
+                  </label>
+                </div>
+              )}
+              <span className="selection"/>
+            </div>
+            {errors.product_color_id && <small style={{color: "red", fontSize: "12px"}}>Оберіть колір плівки</small>}
+            <legend className='legend'>Товщина фасаду:</legend>
+            <div className="radio-input">
+              {productData.prices.map((value, index) =>
+                <label key={index} className="radio-thickness">
                   <input
                     type="radio"
-                    id={color.name}
-                    name="value-radio"
+                    name="thickness"
                     value={Number(index)}
-                    onClick={() => setCurrentColor(color.name)}
-                    {...register("product_color_id", {required: true,})}/>
-                  <span/>
+                    defaultChecked
+                    onClick={() => setPrice(+value.price)}
+                    {...register("product_thickness_id", {required: true})}
+                  />{value.thickness} мм
                 </label>
-              </div>
-            )}
-            <span className="selection"/>
-          </div>
-          {errors.product_color_id && <small style={{color: "red", fontSize: "12px"}}>Оберіть колір плівки</small>}
-          <legend className='legend'>Товщина фасаду:</legend>
-          <div className="radio-input">
-            {productData.prices.map((value, index) =>
-              <label key={index} className="radio-thickness">
-                <input
-                  type="radio"
-                  name="thickness"
-                  value={Number(index)}
-                  defaultChecked
-                  onClick={() => setPrice(+value.price)}
-                  {...register("product_thickness_id", {required: true})}
-                />{value.thickness} мм
-              </label>
-            )}
-          </div>
-          {errors.product_thickness_id && <small style={{color: "red", fontSize: "12px"}}>Оберіть товщину фасаду</small>}
-          <div className="input-wrapper">
-            <div className="input-group">
-              <div className='input-info'>
-                <legend className='legend-size'>Висота фасаду (мм)</legend>
-                <label>
-                  <input
-                    type="text"
-                    name="height"
-                    onInput={handleSetDataForPriceCalc}
-                    {...register(`product_length`, {required: true, pattern: /^\d+$/, min: 300, max: 3000})}
-                  />
-                </label>
-                {errors.product_length && (
-                  <small style={{color: "red", fontSize: "12px"}}>
-                    Мінімальна висота 300, максимальна 3000
-                  </small>
-                )}
-                {errors.product_width && <small style={{visibility: 'hidden'}}>Обов'язкове поле</small>}
-              </div>
-              <div className='input-info'>
-                <legend className='legend-size'>Ширина фасаду (мм)</legend>
-                <label>
-                  <input
-                    type="text"
-                    name="width"
-                    onInput={handleSetDataForPriceCalc}
-                    {...register(`product_width`, {required: true, pattern: /^\d+$/, min: 200, max: 1500})}
-                  />
-                </label>
-                {errors.product_width && (
-                  <small style={{color: "red", fontSize: "12px"}}>
-                    Мінімальна ширина 200, максимальна 1500
-                  </small>
-                )}
-                {errors.product_length && <small style={{visibility: 'hidden'}}>Обов'язкове поле</small>}
-              </div>
-              <div className="counter-wrapper " style={{display: 'flex', flexDirection: 'column'}}>
-                <legend style={{fontSize: '18px', visibility: 'hidden'}}>hidden text</legend>
-                <div className='counter-container'>
-                  <button type='button' className="counter-btn" onClick={handleDecrement}>-</button>
-                  <p ref={countRef}>{dataForPrice.quantity}</p>
-                  <input
-                    type="hidden"
-                    name="count"
-                    {...register("quantity")}
-                  />
-                  <button type='button' className="counter-btn" onClick={handleIncrement}>+</button>
-                </div>
-                  {errors.product_length && <small style={{visibility: 'hidden'}}>Обов'язкове поле</small>}
+              )}
+            </div>
+            {errors.product_thickness_id && <small style={{color: "red", fontSize: "12px"}}>Оберіть товщину фасаду</small>}
+            <div className="input-wrapper">
+              <div className="input-group">
+                <div className='input-info'>
+                  <legend className='legend-size'>Висота фасаду (мм)</legend>
+                  <label>
+                    <input
+                      type="text"
+                      name="height"
+                      onInput={handleSetDataForPriceCalc}
+                      {...register(`product_length`, {required: true, pattern: /^\d+$/, min: 300, max: 3000})}
+                    />
+                  </label>
+                  {errors.product_length && (
+                    <small style={{color: "red", fontSize: "12px"}}>
+                      Мінімальна висота 300, максимальна 3000
+                    </small>
+                  )}
                   {errors.product_width && <small style={{visibility: 'hidden'}}>Обов'язкове поле</small>}
-                  {errors.quantity && <small style={{color: "red", fontSize: "12px"}}>Мінімум 1</small>}
+                </div>
+                <div className='input-info'>
+                  <legend className='legend-size'>Ширина фасаду (мм)</legend>
+                  <label>
+                    <input
+                      type="text"
+                      name="width"
+                      onInput={handleSetDataForPriceCalc}
+                      {...register(`product_width`, {required: true, pattern: /^\d+$/, min: 200, max: 1500})}
+                    />
+                  </label>
+                  {errors.product_width && (
+                    <small style={{color: "red", fontSize: "12px"}}>
+                      Мінімальна ширина 200, максимальна 1500
+                    </small>
+                  )}
+                  {errors.product_length && <small style={{visibility: 'hidden'}}>Обов'язкове поле</small>}
+                </div>
+                <div className="counter-wrapper " style={{display: 'flex', flexDirection: 'column'}}>
+                  <legend style={{fontSize: '18px', visibility: 'hidden'}}>hidden text</legend>
+                  <div className='counter-container'>
+                    <button type='button' className="counter-btn" onClick={handleDecrement}>-</button>
+                    <p ref={countRef}>{dataForPrice.quantity}</p>
+                    <input
+                      type="hidden"
+                      name="count"
+                      {...register("quantity")}
+                    />
+                    <button type='button' className="counter-btn" onClick={handleIncrement}>+</button>
+                  </div>
+                    {errors.product_length && <small style={{visibility: 'hidden'}}>Обов'язкове поле</small>}
+                    {errors.product_width && <small style={{visibility: 'hidden'}}>Обов'язкове поле</small>}
+                    {errors.quantity && <small style={{color: "red", fontSize: "12px"}}>Мінімум 1</small>}
+                </div>
               </div>
             </div>
-          </div>
-          <div className='submit-wrapper'>
-            <button type="submit">Додати в кошик</button>
-            <p className="final-price">{finalPrice ? finalPrice : "0"} грн</p>
-          </div>
-        </>
-      }
-    </form>
+            <div className='submit-wrapper'>
+              <button type="submit">Додати в кошик</button>
+              <p className="final-price">{finalPrice ? finalPrice : "0"} грн</p>
+            </div>
+          </>
+        }
+      </form>
+    </>
   )
 }
