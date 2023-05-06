4e7c992e380a3df6548b4effe8f9ebc6bc3760cb
Pavel <khuzdblacksmith@gmail.com>
2023-05-06 08:51:47 +0300

    number validation upgrade

--- src/validationFields/validation.ts
+++ src/validationFields/validation.ts
@@ -64,7 +64,7 @@ export const validationNumber = (number:string) => {
     if (number === '') return ''; // return 'empty'
     if (!(/^\d\d{8}\d$/gi.test(number) || /^\d\d{10}\d$/gi.test(number))) return 'Некоректний формат даних ';
     if (/^\d\d{8}\d$/gi.test(number)) {
-        if (!(/^0[96][3687]\d{6}\d$/gm.test(number))) {
+        if (!(/^0[965][368705]\d{6}\d$/gm.test(number))) {
             return 'Некоректний формат даних ';
         } else {
 
